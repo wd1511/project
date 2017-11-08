79cbe979db3bc5e665bb4a35bac3b0a8953af971
REKOJNOMED <819849784@qq.com>
2017-11-08 20:39:28 +0800

    初始前端html文件

--- html界面/1.js
+++ html界面/1.js
@@ -1,3 +1,4 @@
+
 function menuManage()
 {
 	var x=document.getElementById('menu');
@@ -9,4 +10,28 @@ function menuManage()
 	{
 		x.style.display="none";
 	}
+}
+
+
+function loginManage()
+{
+	var x=document.getElementById('login_interface');
+	if(x.style.display=="none")
+	{
+		x.style.display="block";
+	}
+	else
+	{
+		x.style.display="none";
+	}
+}
+
+function openLoginModal()
+{
+		$("#loginModal").fadeToggle("fast");
+}
+
+function toggleMenu()
+{
+	$("#menu").fadeToggle("fast");
 }
